9d7aae3602ceba2156c02f93edd456d0ac673d18
Maximilian Huber <huber.maxi@gmail.com>
2019-11-21 11:57:14 +0100

    score

--- app/src/main/java/com/example/astroapp/BallActivity.java
+++ app/src/main/java/com/example/astroapp/BallActivity.java
@@ -7,6 +7,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+
+import android.graphics.Typeface;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -14,15 +16,31 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class BallActivity extends AppCompatActivity {
 
 
     double ax,ay;
+    Random rnd;
+    int lastSpanw=0;
+    public class Asteroid
+    {
+        public double x,y,vx,vy;
+        int r;
+        public Asteroid(double xp, double yp)
+        {
+            x=xp;
+            y=yp;
+            vx=0;
+            vy=10;
+            r=rnd.nextInt(10)+5;
+        }
+    }
+    ArrayList<Asteroid> ast;
     SensorManager sm = null;
     SensorEventListener sel = new SensorEventListener(){
         public void onAccuracyChanged(Sensor sensor, int accuracy) {}
@@ -36,6 +54,8 @@ public class BallActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new MyView(this));
+        rnd = new Random();
+        ast = new ArrayList<>();
         sm = (SensorManager)getSystemService(SENSOR_SERVICE);
         sm.registerListener(sel,  sm.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0), SensorManager.SENSOR_DELAY_NORMAL);
     }
@@ -48,6 +68,8 @@ public class BallActivity extends AppCompatActivity {
         Paint paint = null;
         double xb,yb;
         double vx,vy;
+        double R=50;
+        int iScore=0;
 
         public MyView(Context context) {
             super(context);
@@ -60,6 +82,13 @@ public class BallActivity extends AppCompatActivity {
                     // do stuff then
                     // can call h again after work!
                     BallActivity.MyView.this.invalidate();
+                    lastSpanw+=20;
+                    if(lastSpanw>1000)
+                    {
+                        ast.add(new Asteroid(rnd.nextDouble()*getWidth(),0));
+                        lastSpanw=0;
+                    }
+
                     h.postDelayed(this, 20);
                 }
             }, 1000);
@@ -97,8 +126,25 @@ public class BallActivity extends AppCompatActivity {
             paint.setColor(Color.WHITE);
             canvas.drawPaint(paint);
             // Use Color.parseColor to define HTML colors
-            paint.setColor(Color.parseColor("#ffff00"));
-            canvas.drawCircle((int)xb, (int)yb, 50, paint);
+            paint.setColor(Color.parseColor("#0000ff"));
+            canvas.drawCircle((int)xb, (int)yb,(float)R, paint);
+            paint.setTextSize(30f);
+            canvas.drawText("Score: " + iScore,100,100,paint );
+            paint.setColor(Color.parseColor("#ff0000"));
+            for (Asteroid a: ast)
+            {
+                a.x+=a.vx;
+                a.y+=a.vy;
+                canvas.drawCircle((int)a.x,(int)a.y,a.r,paint);
+            }
+            int i=0;
+            while(i<ast.size())
+            {
+                if(ast.get(i).y>getHeight())
+                    ast.remove(i);
+                else
+                    i++;
+            }
 
         }
     }
