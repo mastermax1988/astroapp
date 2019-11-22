2c4a1609bf8b776cd426487e08663dac0dc2ec01
Maximilian Huber <huber.maxi@gmail.com>
2019-11-22 11:06:16 +0100

    reset highscore

--- app/src/main/java/com/example/astroapp/MainActivity.java
+++ app/src/main/java/com/example/astroapp/MainActivity.java
@@ -7,6 +7,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -23,8 +27,33 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sharedPref = MainActivity.this.getSharedPreferences("default",Context.MODE_PRIVATE);
         int highScore = sharedPref.getInt("highscore", 0);
         TextView txt = findViewById(R.id.txtHighScore);
-        txt.setText("Highscore: "+ highScore);
+        txt.setText("highscore: "+ highScore);
         txt.setTextColor(Color.rgb(0,0,255));
+    }
+    private void resetHighscore()
+    {
+        SharedPreferences sharedPref = MainActivity.this.getSharedPreferences("default",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("highscore", 0);
+        editor.commit();
+        refreshHighscore();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menuReset:
+                resetHighscore();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
